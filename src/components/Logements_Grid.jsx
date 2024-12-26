610df674cb3c10bdf39209de833828bbff1735a6
Vincent Pillot <pillot.vincent@hotmail.com>
2024-12-26 11:06:27 +0100

    Creation d'un component pour l'affichage des logements en grid (pas encore de call API)

---
+++ src/components/Logements_Grid.jsx
@@ -0,0 +1,28 @@
+import '/src/styles/Logements_Grid.css'
+
+function LogementsGrid() {
+    return(
+        <section className="logements__section">
+            <div className="logement">
+                <h2 className="logement__title">Titre</h2>
+            </div>
+            <div className="logement">
+                <h2 className="logement__title">Titre</h2>
+            </div>
+            <div className="logement">
+                <h2 className="logement__title">Titre</h2>
+            </div>
+            <div className="logement">
+                <h2 className="logement__title">Titre</h2>
+            </div>
+            <div className="logement">
+                <h2 className="logement__title">Titre</h2>
+            </div>
+            <div className="logement">
+                <h2 className="logement__title">Titre</h2>
+            </div>                                                            
+        </section>
+    )
+}
+
+export default LogementsGrid
