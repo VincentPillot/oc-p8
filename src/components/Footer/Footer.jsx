aafccdb5634ba82ed2f0e384888cdbe5bd0a24a3
Vincent Pillot <pillot.vincent@hotmail.com>
2025-01-06 11:46:16 +0100

    Deplacement du footer dans un Dossier 'composant' avec son fichier css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import '/src/components/Footer/Footer.css'
+
+function Footer() {
+    return(
+        <footer>
+            <img className="footer__logo" src="/src/assets/images/logo_footer.png" alt="Logo footer" />
+            <p className="footer__copyrights">© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
