026b3183b016d2510476ea2475c607f48edbd71f
Vincent Pillot <pillot.vincent@hotmail.com>
2025-01-13 16:01:50 +0100

    Modification du systeme de routage et ajout de toutes les routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router'
+import {Routes, Route} from 'react-router'
 import './App.css'
 import Homepage from './pages/Homepage'
-import Logement from './pages/Logement'
+import Property from './pages/Property.jsx'
 import About from './pages/About'
 import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Footer from './components/Footer/Footer.jsx'
+import Page404 from './pages/404.jsx'
 import '/src/styles/Style.css'
-
-const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <div>404</div>
-  },
-  {
-    path: '/',
-    element: <Homepage/>
-  },
-  {
-    path: '/logement',
-    element: <Logement/>
-  },
-  {
-    path: '/a-propos',
-    element: <About/>
-  }
-])
-
 
 function App() {
   return(
     <>
       <Navbar/>
-      <RouterProvider router={router} />
+      <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/a-propos" element={<About />} />
+      <Route path="/property" element={<Property />} />
+      <Route path="*" element={<Page404 />} />                  
+      </Routes>
       <Footer/>
     </>
   )
