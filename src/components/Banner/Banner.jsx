db7ee537f0050b4bf557b38e90b1f7997b02ecfc
Vincent Pillot <pillot.vincent@hotmail.com>
2025-01-14 10:44:00 +0100

    Ajout d'une condition pour ne pas generer de h1 si props.title es vide

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,9 +5,9 @@ function Banner(props) {
         <div className="banner" style={{ backgroundImage: `url("${props.image}")` }}>
             <div 
                 className="banner__filter" 
-                style={{ backgroundColor: props.filter ? "rgba(0,0,0,0.6)" : "transparent" }}
+                style={{ backgroundColor: props.filter ? "rgba(0,0,0,0.6)" : "transparent" }} //Change le background filter si props.filter es true ou false
             >
-                <h1>{props.title}</h1>
+                {props.title && <h1>{props.title}</h1>} {/* Condition pour afficher <h1> seulement si props.title est non vide */}
             </div>
         </div>
     );
